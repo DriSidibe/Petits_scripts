0e12c1d845f82ea02737ef6e656a987341d44e64
DriSidibe <dsidibe653@gmail.com>
2022-09-08 16:59:08 -0700

    commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - the main function
+ *
+ *Return: 0 if the program was runnig successfuly
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
